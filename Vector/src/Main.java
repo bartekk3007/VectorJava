59d1016760bb7e8f64bbc0a8ef4e7b6d74eb74c4
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-12-21 23:45:33 +0100

    Add files via upload

---
+++ Vector/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Vector<String> vector = new Vector<>();
+        vector.add("Andrzej");
+        vector.add("Bartek");
+        vector.add("Cezary");
+        System.out.println(vector);
+    }
+}
